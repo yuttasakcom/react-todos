244572b378c03d81238ed3e11696c89c3e2a7fa5
YoProgrammer <yuttasakcom@gmail.com>
2018-02-09 14:11:38 +0700

    add style

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -22,6 +22,10 @@ export default {
         loader: 'babel-loader',
         include: [path.join(__dirname, 'src')],
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
